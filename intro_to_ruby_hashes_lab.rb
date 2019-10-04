809fce33f2a03053fdcec62ae44aa658e283dac9
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-04 01:21:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-  return {genre:, "Punk"}# return a valid hash with any key/value pair of your choice
+  return {:genre, "Punk"}# return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
